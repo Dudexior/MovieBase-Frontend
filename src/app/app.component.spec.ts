821c4a1d92fda99d7daf51551da3cbabe5248411
dudexior <dawid@tomczakd.pl>
2021-03-14 15:10:48 +0100

    changed title and favicon

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,12 +20,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'moviebase'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('moviebase');
-  });
-
   it('should have header', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
