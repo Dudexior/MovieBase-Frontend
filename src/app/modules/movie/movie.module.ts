bd6ec7f6693ca7e19b6361b60671c9804d723e8f
dudexior <dawid@tomczakd.pl>
2021-03-15 18:23:51 +0100

    created basic movie service

--- src/app/modules/movie/movie.module.ts
+++ src/app/modules/movie/movie.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { MovieRoutingModule } from './movie-routing.module';
 import { MoviePageComponent } from './components/movie-page/movie-page.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [MoviePageComponent],
   imports: [
     CommonModule,
-    MovieRoutingModule
+    MovieRoutingModule,
+    HttpClientModule,
   ]
 })
 export class MovieModule { }
---
+++ src/app/modules/movie/services/movie.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Movie } from '../models/movie';
+import { GET_ALL_MOVIES } from 'src/app/shared/modules/consts';
+import { take } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  constructor(private http: HttpClient) { }
+
+  downloadMovies(): Observable<Movie[]> {
+    const url = GET_ALL_MOVIES;
+
+    return this.http.get<Movie[]>(url).pipe(take(1));
+  }
+}
