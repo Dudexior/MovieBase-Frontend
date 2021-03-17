d9e72f77613a0b8977ab06bf42612ca133016f86
dudexior <dawid@tomczakd.pl>
2021-03-17 16:16:53 +0100

    added movie details page to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MovieDetailsPageComponent } from './modules/movie/components/movie-details-page/movie-details-page.component';
 import { MoviePageComponent } from './modules/movie/components/movie-page/movie-page.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MoviePageComponent
+  },
+  {
+    path: 'movie/:id',
+    component: MovieDetailsPageComponent
   },
   {
     path: '**',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  backendURL: 'https://localhost:44366'
+  backendURL: 'https://moviebaseapi.azurewebsites.net'
 };
 
 /*
