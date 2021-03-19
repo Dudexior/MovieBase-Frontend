d3cfc7fdbb4cc9d022f5d8526d2796bfd009021b
dudexior <dawid@tomczakd.pl>
2021-03-19 11:03:00 +0100

    created image service

---
+++ src/app/modules/movie/services/image.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { MOVIES_CONTROLLER_ID_IMAGE } from 'src/app/shared/consts';
+import { Movie } from '../models/movie';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageService {
+
+  constructor(private http: HttpClient) { }
+
+  patchImage(movieId: number, image: File): Observable<Movie> {
+    const url = MOVIES_CONTROLLER_ID_IMAGE.replace(/:ID/, movieId.toString());
+
+    const body = new FormData();
+    body.append('file', image);
+
+    return this.http.patch<Movie>(url, body);
+  }
+}
--- src/app/shared/consts.ts
+++ src/app/shared/consts.ts
@@ -4,6 +4,7 @@ export const BASE_URL = environment.backendURL;
 
 export const MOVIES_CONTROLLER = BASE_URL + '/Movies';
 export const MOVIES_CONTROLLER_ID = MOVIES_CONTROLLER + '/:ID';
+export const MOVIES_CONTROLLER_ID_IMAGE = MOVIES_CONTROLLER_ID + '/Image';
 
 export const DISPLAYS_CONTROLLER = BASE_URL + '/Displays';
 export const DISPLAYS_CONTROLLER_ID = DISPLAYS_CONTROLLER + '/:ID';
